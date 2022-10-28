fb298babba96097bc0289eebe8a0a194a661439d
Ayden Overson <ayden99@gmail.com>
2022-10-27 21:26:11 -0700

    good enough

--- app/routes.py
+++ app/routes.py
@@ -4,7 +4,7 @@ from flask import render_template
 
 @app.route('/')
 def home_page():
-    return render_template('index.html')
+    return render_template('home.html')
 
 @app.route('/favorites')
 def favorites_page():
@@ -16,3 +16,6 @@ def favorites_page():
         {'name' : 'Rainbow Kitten Suprise', 'song' : 'When it lands'}
     ]
     return render_template('favorites.html', favartists = favartists)
+
+
+    # https://media.npr.org/assets/img/2020/06/05/rtj-by-tim-saccenti-2_wide-db330e92d4712fb8374a3ba7ff522be91325c1bd-s1400-c100.jpg
